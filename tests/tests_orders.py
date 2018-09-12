a9bb433ddf12299903c9687c6cf7741ca8b57fcd
novak254 <ndemokelvinonkundi@gmail.com>
2018-09-12 06:00:18 +0300

    g

--- tests/tests_orders.py
+++ tests/tests_orders.py
@@ -1,11 +1,12 @@
-from unittest import TestCase
+import unittest
 import json
 from app import create_app
 
 
-class TestOrders(TestCase):
-
-    def SetUp(self):
+class TestOrders(unittest.TestCase):
+    '''set up for testing'''
+    
+    def setUp(self):
         self.app = create_app("testing")
         self.client = self.app.test_client()
         self.app_context = self.app.app_context()
@@ -16,37 +17,67 @@ class TestOrders(TestCase):
 
     def test_create_order(self):
         data = {
-            "name":"maiza",
-            "price":20,
-            "description":"sweet ugali"
+            "name": "eggcurry",
+            "price": 20,
+            "description": "sweet eggs"
         }
 
-        resp = self.client.post(
+        res = self.client.post(
             "/api/v1/orders",
-            data = json.dumps(data),
-            headers = {"content-type":"application/json"}
+            data=json.dumps(data),
+            headers={"content-type": "application/json"}
         )
 
-        self.assertEqual(resp.status_code,201)
-        self.assertEqual(json.loads(resp.data)['message'],"status approved")
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(json.loads(res.data)['message'], "Food order created")
 
-        def test_get_order_by_id(self):
-            resp = self.client.get(
-                "api/v1/orders/1",
-                headers = {"content-type":"application/json"}
-            )
+    def test_get_all_orders(self):
 
-            self.assertEqual(resp.status_code,200)
+        res = self.client.get(
+            "/api/v1/orders",
+            headers={"content-type": "application/json"}
+        )
 
-        def test_update_order_status(self):
-            resp = self.client.put(
-                "api/v1/orders/1",
-                headers = {"content-type":"application/json"}
-            )
+        self.assertEqual(res.status_code, 200)
 
-            self.assertEqual(resp.status_code,200)
-            self.assertEqual(json.loads(resp.data)['message'],"status approved")
 
-            
+    def test_order_by_id(self):
+        res = self.client.get(
+            "/api/v1/orders/1",
+            headers={"content-type": "application/json"}
+        )
+        self.assertEqual(res.status_code, 200)
 
-        
+    
+    def test_update_order_status(self):
+        res = self.client.put(
+            "api/v1/orders/1",
+            headers={"content-type": "application/json"}
+        )
+        print(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(json.loads(res.data)[
+                         'message'], "status approved")
+
+    
+    
+
+
+    def test_non_order_by_id(self):
+        res = self.client.get(
+            "/api/v1/orders/111",
+            headers={"content-type": "application/json"}
+        )
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(json.loads(res.data)[
+                         'message'], "Order not found")
+
+
+    def test_non_order_delete(self):
+        res = self.client.delete(
+            "api/v1/orders/11",
+            headers={"content-type": "application/json"}
+        )
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(json.loads(res.data)[
+                         'message'], "Order not found")
